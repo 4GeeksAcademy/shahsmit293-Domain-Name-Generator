8eef841f4ba36cc773a933af09fab184eac40fb2
smit shah <shahsmit293@gmail.com>
2023-08-08 00:51:27 +0000

    Intitial Change

--- src/app.js
+++ src/app.js
@@ -1,11 +1,62 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//main project start here.....
+let n1 = 0;
+for (
+  pronounIndex = 0;
+  pronounIndex < pronoun.length;
+  pronounIndex = pronounIndex + 1
+) {
+  for (adjIndex = 0; adjIndex < adj.length; adjIndex = adjIndex + 1) {
+    for (nounIndex = 0; nounIndex < noun.length; nounIndex = nounIndex + 1) {
+      n1 = n1 + 1;
+      var domain = `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}`;
+      var extension = `${n1} ${domain}.com`;
+      console.log(extension);
+    }
+  }
+}
+console.log("\n");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Add several types of extensions: .com, .net, .us, .io, etc.
+let n2 = 0;
+for (
+  pronounIndex = 0;
+  pronounIndex < pronoun.length;
+  pronounIndex = pronounIndex + 1
+) {
+  for (adjIndex = 0; adjIndex < adj.length; adjIndex = adjIndex + 1) {
+    for (nounIndex = 0; nounIndex < noun.length; nounIndex = nounIndex + 1) {
+      n2 = n2 + 1;
+      var domain = `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}`;
+      var extension = `${n2} ${domain}.net`;
+      console.log(extension);
+    }
+  }
+}
+console.log("\n");
+
+//Add domain hacks, for example: instead of thegreatjogger.com you can use the .er domain like this: thegreatjogg.er
+let number = 0;
+for (
+  pronounIndex = 0;
+  pronounIndex < pronoun.length;
+  pronounIndex = pronounIndex + 1
+) {
+  for (adjIndex = 0; adjIndex < adj.length; adjIndex = adjIndex + 1) {
+    for (nounIndex = 0; nounIndex < noun.length; nounIndex = nounIndex + 1) {
+      number = number + 1;
+      var domain = `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}`;
+      var extension = `${number} ${domain}.com`;
+      console.log(extension);
+      if (extension.endsWith("er.com")) {
+        let txt = extension.slice(0, -6);
+        let sliceDomain = domain.slice(-2);
+        let updatedExtension = `${txt}.${sliceDomain}`;
+        console.log(updatedExtension);
+      } 
+    }
+  }
+}
